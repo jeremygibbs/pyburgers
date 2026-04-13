Fixed inconsistency with derivative calls

pyburgers/utils/spectral.py
@@ -178,22 +178,22 @@ class Derivatives:
 
         # Loop through requested derivative orders
         for key in orders:
-            if key == 1:
+            if key == 1 or key == "1":
                 self.fun[:] = 1j * self.k * fu
                 self.ifft()
                 np.multiply(self.fac, self.der, out=self._out_1)
                 derivatives["1"] = self._out_1
-            elif key == 2:
+            elif key == 2 or key == "2":
                 self.fun[:] = -self.k2 * fu
                 self.ifft()
                 np.multiply(self.fac2, self.der, out=self._out_2)
                 derivatives["2"] = self._out_2
-            elif key == 3:
+            elif key == 3 or key == "3":
                 self.fun[:] = -1j * self.k3 * fu
                 self.ifft()
                 np.multiply(self.fac3, self.der, out=self._out_3)
                 derivatives["3"] = self._out_3
-            elif key == 4:
+            elif key == 4 or key == "4":
                 self.fun[:] = self.k4 * fu
                 self.ifft()
                 np.multiply(self.fac4, self.der, out=self._out_4)